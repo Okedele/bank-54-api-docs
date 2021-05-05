fcf75a8ad26f6b85005f04f342d3ee4e29d079a6
Okedele <okedelep@gmail.com>
2021-05-05 21:14:30 +0100

    added documentation for auth, user, user roles, partner and customer

--- config.rb
+++ config.rb
@@ -54,6 +54,7 @@ configure :build do
   # activate :gzip
 end
 
+activate :directory_indexes
 # Deploy Configuration
 # If you want Middleman to listen on a different port, you can set that below
 set :port, 4567
